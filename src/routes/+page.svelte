799175540c280f1a9471d31afbb5fe50083ce0d0
the-okapi <theo_kapi@outlook.com>
2025-07-31 17:02:37 -0700

    #13 Old Home Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+    import { goto } from '$app/navigation';
 	import { Input, Label, Button } from '$lib/components';
 	import { getFile, pb } from '$lib/pocketbase';
 	import type { Result } from '$lib/pocketbase';
@@ -28,7 +28,7 @@
 		} else {
 			loading = false;
 			if (result.value === 'Failed to authenticate.') {
-				errorText = 'Invalid username or password.';
+				errorText = 'Invalid file code or password.';
 			} else {
 				errorText = 'Error: ' + result.value;
 			}
@@ -71,20 +71,16 @@
 	<title>Repaper</title>
 </svelte:head>
 
-<h1 class="mt-[23vh] h-[14vh] text-center text-[4em] font-black select-none">Repaper</h1>
+<h1 class="mt-[18vh] h-[14vh] text-center text-[4em] font-black select-none">Repaper</h1>
 <p class="text-center font-bold text-red-500">
 	<span class="text-transparent">m</span>{errorText}<span class="text-transparent">m</span>
 </p>
 {#if loading}
 	<p class="text-center font-bold text-[#00bfff]">Loading...</p>
 {:else}
-	<div class="funGrid grid text-center">
-		<div class="mt-[24vh]">
-			<Button onclick={createFile} class="text-md h-12">Create File</Button>
-		</div>
-		<div class="vl"></div>
-		<div class="my-[17vh]">
-			{#if !loading}
+	<div class="text-center">
+		{#if !loading}
+			<div class="mt-[11vh]">
 				<form {onsubmit} class="m-auto block w-fit align-middle">
 					<Label
 						for="fileCode"
@@ -112,8 +108,10 @@
 						<Button class="text-md ml-2 h-12 w-14" type="submit" {disabled}>Go</Button>
 					</div>
 				</form>
-			{/if}
-		</div>
+			</div>
+            <hr class="my-[5vh] w-[50vw] m-auto">
+		    <Button onclick={createFile} class="text-md h-12">Create File</Button>
+		{/if}
 	</div>
 {/if}
 
@@ -123,13 +121,5 @@
 			0 0 10px #00bfff,
 			0 0 20px #00bfff,
 			0 0 30px #00bfff;
-	}
-	.funGrid {
-		grid-template-columns: 5fr 1fr 5fr;
-	}
-	.vl {
-		height: 53vh;
-		border-left: 1px solid white;
-		margin: auto;
 	}
 </style>
